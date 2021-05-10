5ff55381896f23503a98a29056ca477b443642a6
Josh Lavallee <44557318+jlavallee76@users.noreply.github.com>
2021-05-10 14:13:35 -0500

    Added ityped, finished intro section

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Contact from './components/contact/Contact'
 import {useState} from "react"
 
 function App() {
-  const [menuOpen, setMenuOpen] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import { useEffect, useRef} from 'react'
+import { init } from 'ityped'
 import "./intro.scss"
 
-export default function intro() {
+export default function Intro() {
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            strings: ["Developer", "Photographer", "Traveller", "Sports Fan", "Fantasy Football Legend", "Dog Dad"]
+        })
+    }, [])
     return (
         <div className="intro" id="intro">
-            Intro
+            <div className="left">
+                <div className="imageContainer">
+                    <img src="assets/logo.png" alt="logo" />
+                </div>
+            </div>
+
+            <div className="right">
+                <div className="wrapper">
+                    <h1>Hi, I'm Josh!</h1>
+                    <h3><span ref={textRef}></span></h3>
+                </div>
+                <a href="#about">
+                    <img src="assets/down.png" alt="down arrow" />
+                </a>
+            </div>
         </div>
     )
 }
