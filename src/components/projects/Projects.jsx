a8dd34108769e3b9f8f65afc32eb1e25e60bf245
Josh Lavallee <44557318+jlavallee76@users.noreply.github.com>
2021-05-10 18:05:12 -0500

    Added projects section with some item containers. Included transitions

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,7 +4,60 @@ import "./projects.scss"
 export default function projects() {
     return (
         <div className="projects" id="projects">
-            Projects
+            <h1>Projects</h1>
+            <ul>
+                <li className="active">Most Recent</li>
+                <li>Mobile Applications</li>
+                <li>Web Applications</li>
+                <li>Photography</li>
+            </ul>
+
+            <div className="container">
+                <div className="item">
+                    <img src="https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png" alt="palceholder" />
+                    <h3>
+                        <a href="https://github.com/jlavallee76/Water-Assessment-Project">Water Assessment Project</a>
+                    </h3>
+                </div>
+                <div className="item">
+                    <img src="https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png" alt="palceholder" />
+                    <h3>
+                        <a href="https://github.com/jlavallee76/Crockodile">Crockodile</a>
+                    </h3>
+                </div>
+                <div className="item">
+                    <img src="https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png" alt="palceholder" />
+                    <h3>Water Assessment Project</h3>
+                </div>
+                <div className="item">
+                    <img src="https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png" alt="palceholder" />
+                    <h3>Water Assessment Project</h3>
+                </div>
+                <div className="item">
+                    <img src="https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png" alt="palceholder" />
+                    <h3>Water Assessment Project</h3>
+                </div>
+                <div className="item">
+                    <img src="https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png" alt="palceholder" />
+                    <h3>Water Assessment Project</h3>
+                </div>
+                <div className="item">
+                    <img src="https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png" alt="palceholder" />
+                    <h3>Water Assessment Project</h3>
+                </div>
+                <div className="item">
+                    <img src="https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png" alt="palceholder" />
+                    <h3>Water Assessment Project</h3>
+                </div>
+                <div className="item">
+                    <img src="https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png" alt="palceholder" />
+                    <h3>Water Assessment Project</h3>
+                </div>
+                <div className="item">
+                    <img src="https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png" alt="palceholder" />
+                    <h3>Water Assessment Project</h3>
+                </div>
+            </div>
         </div>
     )
 }
