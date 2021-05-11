d8d0148979c46b9d17855dd09e456fd95aea7b3e
Josh Lavallee <44557318+jlavallee76@users.noreply.github.com>
2021-05-11 12:13:14 -0500

    Added resume cards and slider with dynamic data from js file

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,9 +1,84 @@
 import "./resume.scss"
+import { resumeData } from '../../resumeData'
 
-export default function resume() {
+export default function Resume() {
+    // const data = resumeData
     return (
         <div className="resume" id="resume">
-            Resume
+            <div className="slider">
+                {resumeData.map((item) => 
+                    <div className="container">
+                        <div className="item">
+                            <div className="left">
+                                <div className="leftContainer">
+                                    <div className="imgContainer">
+                                        <img src={item.icon} alt={item.title} />
+                                    </div>
+                                    <h2>{item.title}</h2>
+                                    <p>{item.desciption}</p>
+                                    <span><a href="{item.url}">{item.urldesc}</a></span>
+                                </div>
+                            </div>
+
+                            <div className="right">
+                                {item.info.map((detail) =>
+                                <div className="container">
+                                    <h2>{detail.name}</h2>
+                                    <h3>{detail.date}</h3>
+                                    <ul>
+                                        {detail.details.map((desc) =>
+                                        <li>{desc.description}</li> )}
+                                    </ul>
+                                </div> )}
+                            </div>     
+                        </div>
+                    </div>
+                )}
+                {/* <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src="assets/education.png" alt="" />
+                                </div>
+                                <h2>Red River College</h2>
+                                <p>Red River has been accredited by the Canadian Information Processing Society's (CIPS) College Program Accreditation Council (CPAC). The CPAC works with academic institutions to ensure that educational programs prepare students for the demands of the Information System profession.</p>
+                                <span><a href="https://catalogue.rrc.ca/Programs/WPG/Fulltime/BUSGF-DP">More About The Program</a></span>
+                            </div>
+                        </div>
+
+                        <div className="right">
+                            <div className="container">
+                                <h2>Business Information Technology</h2>
+                                <h3>Diploma | GPA 4.45 | January 2019 - April 2021</h3>
+                                <ul>
+                                    <li>Web Development (HTML, CSS, JavaScript, PHP, Ruby, & MySQL)</li>
+                                    <li>Data Structures & Algorithms</li>
+                                    <li>Programming (Java & C#)</li>
+                                    <li>Database (SQL)</li>
+                                    <li>Object-Oriented Analysis & Design (UP, UML, & Agile Methodologies)</li>
+                                    <li>Business Issues & Management</li>
+                                    <li>Financial Accounting & Statistics</li>
+                                    <li>Communications & Professional Development</li>
+                                </ul>
+                            </div>
+
+                            <div className="container">
+                                <h2>Introduction to Business Information Technology</h2>
+                                <h3>Certificate | GPA 4.50 | September 2018 - December 2019</h3>
+                                <ul>
+                                    <li>Introduction to Programming Logic</li>
+                                    <li>Foundations for Success</li>
+                                    <li>Business Computing</li>
+                                    <li>Mathematics for Business Information Techonology</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div> */}
+            </div>
+            <img className="left arrow" src="assets/left.png" alt="left arrow" />
+            <img className="right arrow" src="assets/right.png" alt="right arrow" />
         </div>
     )
 }
---
+++ src/resumeData.js
@@ -0,0 +1,154 @@
+export const resumeData = [
+    {
+        id: 1,
+        icon: "assets/education.png",
+        title: "Red River College",
+        desciption: "Red River has been accredited by the Canadian Information Processing Society's (CIPS) College Program Accreditation Council (CPAC). The CPAC works with academic institutions to ensure that educational programs prepare students for the demands of the Information System profession.",
+        url: "https://catalogue.rrc.ca/Programs/WPG/Fulltime/BUSGF-DP",
+        urldesc: "More About The Program",
+        info: [
+            {
+                id: 1,
+                name: "Business Information Technology",
+                date: "Diploma | GPA 4.45 | January 2019 - April 2021",
+                details: [
+                    {
+                        id: 1,
+                        description: "Web Development (HTML, CSS, JavaScript, PHP, Ruby, & MySQL)"
+                    },
+                    {
+                        id: 2,
+                        description: "Data Structures & Algorithms"
+                    },
+                    {
+                        id: 3,
+                        description: "Programming (Java & C#)"
+                    },
+                    {
+                        id: 4,
+                        description: "Database (SQL)"
+                    },
+                    {
+                        id: 5,
+                        description: "Object-Oriented Analysis & Design (UP, UML, & Agile Methodologies)"
+                    },
+                    {
+                        id: 6,
+                        description: "Business Issues & Management"
+                    },
+                    {
+                        id: 7,
+                        description: "Financial Accounting & Statistics"
+                    },
+                    {
+                        id: 8,
+                        description: "Communications & Professional Development"
+                    }
+                ]
+            },
+            {
+                id: 1,
+                name: "Introduction Business Information Technology",
+                date: "Certificate | GPA 4.50 | September 2018 - December 2018",
+                details: [
+                    {
+                        id: 1,
+                        description: "Introduction to Programming Logic"
+                    },
+                    {
+                        id: 2,
+                        description: "Foundations for Success"
+                    },
+                    {
+                        id: 3,
+                        description: "Business Computing"
+                    },
+                    {
+                        id: 4,
+                        description: "Mathematics for Business Information Techonology"
+                    }
+                ]
+            }]
+    },
+    {
+        id: 2,
+        icon: "assets/award.png",
+        title: "Acheivements",
+        desciption: "Some type of description",
+        url: "https://catalogue.rrc.ca/Programs/WPG/Fulltime/BUSGF-DP/AwardsandScholarships",
+        info: [
+            {
+                id: 1,
+                name: "Outstanding Academic Acheivement Award",
+                date: "June 2019",
+                details: [
+                    {
+                        id: 1,
+                        desciption: "Presented annually, each spring, to students who have completed an Applied Computer Education program selected on the basis of outstanding academic achievement." 
+                    }
+                ]
+            }]
+    },
+    {
+        id: 3,
+        icon: "assets/volunteering.png",
+        title: "Volunteer Experience",
+        desciption: "Some type of description",
+        url: "",
+        info: [
+            {
+                id: 1,
+                name: "Koats For Kids",
+                date: "November 2020",
+                details: [
+                    {
+                        id: 1,
+                        desciption: "Unloaded product from shipping containers" 
+                    },
+                    {
+                        id: 2,
+                        desciption: "Laundered items of clothing and prepared them for sorting" 
+                    },
+                    {
+                        id: 3,
+                        desciption: "Organized, packaged and prepared winter bundles for delivery" 
+                    }
+                ]
+            }]
+    },
+    {
+        id: 4,
+        icon: "assets/workexp.png",
+        title: "Work Experience",
+        desciption: "Some type of description",
+        url: "",
+        info: [
+            {
+                id: 1,
+                name: "Bold Commerce",
+                date: "Software Developer | September 2020 - December 2020",
+                details: [
+                    {
+                        id: 1,
+                        desciption: "Maintained applications in the Shopify ecosystem adhering to high coding standards using JavaScript and PHP" 
+                    },
+                    {
+                        id: 2,
+                        desciption: "Developed E2E tests for e-commerce applications using CodeceptJS" 
+                    },
+                    {
+                        id: 3,
+                        desciption: "Produced clear and concise user documentation using Confluence" 
+                    },
+                    {
+                        id: 4,
+                        desciption: "Participated in and led team standup meetings & retrospectives" 
+                    },
+                    {
+                        id: 4,
+                        desciption: "Applied agile methodologies to projects daily to work rapidly as part of a team and as an individual" 
+                    }
+                ]
+            }]
+    }
+];
